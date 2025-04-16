e8c76cd6c2eb5e62623e756541351a648ea35a5a
MalcolmX2145 <ekajjamalcolm@gmail.com>
2025-04-16 11:59:55 +0300

    Set up authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+"use client";
+import { SignInButton, SignOutButton, SignedIn, SignedOut } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      HomePage
+
+      {/* Check if user is signed out and if so show signinbutton */}
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+
+      {/* Check if user is signed in and if so show signoutbutton */}
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
   )
 }
 
